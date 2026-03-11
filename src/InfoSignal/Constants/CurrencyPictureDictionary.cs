5a4dbe8f5fae967e369752f3ffbb8ba2206ab812
Ruh <ruh@tut.by>
2026-03-11 11:32:56 +0300

    init: add some classes.

---
+++ src/InfoSignal/Constants/CurrencyPictureDictionary.cs
@@ -0,0 +1,12 @@
+﻿namespace InfoSignal.Constants
+{
+	public static class CurrencyPictureDictionary
+	{
+		public static Dictionary<string, string> Pictures = new()
+		{
+			{ "USD", "💵" },
+			{ "EUR", "💶" },
+			{ "RUB", "₽" }			
+		};
+	}
+}
--- src/InfoSignal/Interfaces/IMailService.cs
+++ src/InfoSignal/Interfaces/IMailService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace InfoSignal.Interfaces
+﻿namespace InfoSignal.Interfaces
 {
 	public interface IMailService
 	{
